1cdee3ed90b4f5656906e2ffd2b7bab3da0ddec9
tofutsai <jason780625@gmail.com>
2021-05-19 21:39:21 +0800

    addSysConfig

--- WebStock/ViewModels/stockStatisticsViewModel.cs
+++ WebStock/ViewModels/stockStatisticsViewModel.cs
@@ -8,6 +8,11 @@ namespace WebStock.ViewModels
 {
     public class stockStatisticsViewModel
     {
+        public int searchOper { get; set; }
+        public string searchType { get; set; }
+        public string searchCode { get; set; }
+        public int searchShares { get; set; }
+        public double searchClosingPrice { get; set; }
         public List<stockSummaryStatistics> stockSummaryStatistics { get; set; }
     }
 }
